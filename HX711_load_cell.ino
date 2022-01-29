fe5cb78176c97c3ffe4c47e007224edfcb6b031d
Derek123454 <dmfronek@gmail.com>
2022-01-29 16:16:59 -0500

    Update HX711_load_cell.ino

--- HX711_load_cell.ino
+++ HX711_load_cell.ino
@@ -15,21 +15,38 @@ void setup() {
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   // Start the I2C Bus as Slave on address 9
   Wire.begin(9); 
+  Wire.setClock(400000);
    // Attach a function to trigger when something is received.
   Wire.onRequest(requestEvent);
 }
 
+
+
+
+
+    
 void requestEvent() {
-    long test = 7777;    
-    Wire.write(test); //return a long (4 bytes to robo rio)   
+    byte buf[4] = {};
+
+    buf[0] = filtered_data >> 24; //shift the data bitwise into their correct spot in the byte array
+    buf[1] = filtered_data >> 16;
+    buf[2] = filtered_data >> 8;
+    buf[3] = filtered_data; 
+    
+    Wire.write(buf, sizeof(buf)); //return a long (4 bytes to robo rio)   
 }
 void loop() {
-
+  
   if (scale.is_ready()) {
     long raw = scale.read();
-    filtered_data = adcFilter1.filter(raw);    
+    filtered_data = adcFilter1.filter(raw); 
+    //Wire.beginTransmission(112);
+    //Wire.write(test);
+    //Wire.endTransmission();   
     Serial.println(filtered_data);
     //Serial.println(filtered2);
+
+    
 }
-  //delay(500);
+    delay(500);
 }
